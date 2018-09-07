58dfafcce6d62a6bc93ddd9a3efab4e6bed28d1e
Szymon Grabowski <szmiko@wp.pl>
2018-09-07 23:12:24 +0200

    Upgrade

--- script.js
+++ script.js
@@ -13,4 +13,13 @@ function showAnswer(event) {
 	var name = image.id;
 	name = name + ".jpg";
 	image.src = name;
+
+	setTimeout(reblur, 2000, image);
+};
+
+function reblur(image) {
+	var name = image.id;
+	name = name + "blur.jpg";
+
+	image.src = name;
 };
